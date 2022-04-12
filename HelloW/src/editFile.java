dffc6c7598eb34c477e0c0617416ff7dfb9a683b
LitLeo <67174309+MarioBelansky@users.noreply.github.com>
2022-04-12 15:44:22 +0200

    Update editFile.java

--- HelloW/src/editFile.java
+++ HelloW/src/editFile.java
@@ -16,16 +16,14 @@ public void edit(String read,String write){
 
             while ((strCurrentLine = objReader.readLine()) != null) {
                 try {
-
                     String data0 =strCurrentLine+" ";
-                    String data1 = data0.replaceAll("\\s+", "\n").toLowerCase(Locale.ROOT);
-
-                    //remove characters,digits,],[,",plural,
-                    String dataFIN = data1.replaceAll("[{,.(’‘`'):\\[*©\\];`#£\"“\\d§%$–!?&}_-]", "")
+                    //remove characters,digits,],[,",
+                    String data1 = data0.replaceAll("[{,.(’‘`'):\\[*©\\];`#£\"“\\d§%$–!?&}_-]", " ")
                             .replaceAll("\uF733","").replaceAll("\uF731","");
+                    //line breaks
+                    String dataFIN = data1.replaceAll("\\s+", "\n").toLowerCase(Locale.ROOT);
                     // Writes the string to the file
                     objWriter.write(dataFIN);
-
                 }catch (Exception e) {
                     e.getStackTrace();
                 }
